bcc1fa898743fa4b2e47db586c240644e9671a3b
David Janda <david.janda02@upol.cz>
2023-05-25 22:50:07 +0200

    Create separate datasets for training and testing Added visualize function Added model evaluation Added model prediction Added model predictio

--- Script.r
+++ Script.r
@@ -2,18 +2,50 @@
 renv::init() ### For more info check https://rstudio.github.io/renv/articles/renv.html # nolint: line_length_linter.
 
 library(tidyverse)
-
-install.packages('httpgd')
+library(ggplot2)
 library(httpgd) ### For better plots in VS code
-
+library(reshape2)
 
 renv::snapshot() ### To save the environment
 
-### Data from kaggle digit recognizer
-train_data <- read.csv('data/train.csv')
-test_data <- read.csv('data/test.csv')
+### Data from kaggle   
+train <- read.csv('data/train.csv')
+test <- read.csv('data/test.csv')
+
+#### Training datasets ####
+xtrain <- train[1:floor(nrow(train)*0.6),]
+xtest <- train[(floor(nrow(train)*0.8)+1):nrow(train),]
+xval <- train[(floor(nrow(train) * 0.6) + 1) : floor(nrow(train) * 0.8),]
 
 
-train <- as.matrix(train_data)  #### Create matrix to do the math
-train <- as.array(train)
+train[train == 0] <- 10
+ytrain <- train[1:floor(nrow(train)*0.6), 1]
+yval <- train[(floor(nrow(train)*0.6) + 1):floor(nrow(train)*0.8), 1]
+ytest <- train[(floor(nrow(train)*0.8) + 1):nrow(train), 1]
+
+
+### visualizating function ###
+# visualize recreates a square image from the unrolled matrix that represents it
+
+visualize <- function(imgvec) {
+
+    n <- length(imgvec)
+    
+    # Reshape the vector into a matrix
+    img <- matrix(imgvec, sqrt(n))
+    
+    # Reformat the data for plotting
+    imgmelt <- melt(img)
+    
+    p <- ggplot(imgmelt, aes(x = Var1, y = -Var2 + sqrt(n) + 1)) +
+        geom_raster(aes(fill = imgmelt$value)) +
+        scale_fill_gradient(low = "white", high = "black", guide = FALSE) +
+        labs(x = NULL, y = NULL, fill = NULL)
+  
+    print(p)       
+}
+
+visualize(
+    as.numeric(xtrain[10, -1])
+)
 
